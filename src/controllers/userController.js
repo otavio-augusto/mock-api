782271a9783cf1fa53022b49b4c1f12c5fc26e6b
Otávio Augusto <oaasouza@sga.pucminas.br>
2022-07-27 19:27:15 -0300

    feat: outlining userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,36 +1,32 @@
-//ROTA GET USER PLURAL
+//ROTA GET PLURAL
 export function getUsers(req, res, next) {
   const message = {
     "message": "PLURAL GET ACKNOWLEDGED!"
   }
 
   res.status(201).send(message);
-  //TODO GET PLURAL
 };
 
 //ROTA GET SINGULAR
 export function getUser(req, res, next) {
   const id = req.params.id
   const message = {
-    "message": "SINGULAR GET ACKNOWLEDGED!"
+    "message": `SINGULAR GET ACKNOWLEDGED!`,
+    "id": id
   }
   res.status(201).send(message);
-  //TODO GET SINGULAR
 };
 
 export function putUser(req, res, next) {
   res.status(201).send('Requisição recebida com sucesso!');
-  //TODO PUT
 };
 
 export function postUser(req, res, next) {
   let id = req.params.id;
   res.status(201).send(`Requisição recebida com sucesso! ${id}`);
-  //TODO POST
 };
 
 export function deleteUser(req, res, next) {
   let id = req.params.id;
   res.status(200).send(`Requisição recebida com sucesso! ${id}`);
-  //TODO DELETE
 };
