d13b0826cce6512ddbcaa407b805a303f0212d7b
Souza <otavioaas@outlook.com>
2022-08-26 15:21:42 -0300

    feat: migration para themes

---
+++ src/migration/1661537804739-CreateTheme.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateTheme1661537804739 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "theme",
+                columns: [
+                    {
+                        name: "id",
+                        type: "serial4",
+                    },
+                    {
+                        name: "primary",
+                        type: "varchar(200)"
+                    },
+                    {
+                        name: "secondary",
+                        type: "varchar(200)"
+                    },
+                    {
+                        name: "tertiary",
+                        type: "varchar(200)"
+                    },
+                    {
+                        name: "alert",
+                        type: "varchar(200)"
+                    },
+                ]
+            })
+        );
+    }
+
+    public async down(_queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
