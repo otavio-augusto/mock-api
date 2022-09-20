9e2afe5af750856be3aae3ca388c5e0bd6b8fd1f
Souza <otavioaas@outlook.com>
2022-09-20 09:41:37 -0300

    fix: cookie não sendo lido para retirar autorização

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,12 +2,15 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
-
+import { Request } from 'express';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                JwtStrategy.validateCookieAuth,
+                ExtractJwt.fromAuthHeaderAsBearerToken()
+            ]),
             ignoreExpiration: false,
             secretOrKey: jwtConstants.secret,
         });
@@ -15,5 +18,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     async validate(payload: any) {
         return { userId: payload.id, username: payload.username };
+    }
+
+    private static validateCookieAuth(request: Request) {
+        console.log(request.cookies)
+        if (
+            request.cookies &&
+            'auth-cookie' in request.cookies &&
+            request.cookies['auth-cookie'].length > 0
+        )
+            return request.cookies['auth-cookie']
+        return null
     }
 }
